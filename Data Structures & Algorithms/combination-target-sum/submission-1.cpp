479f458d32aec6049da66956584c608896dca2c4
Harsh Dobariya <84761837+HarshDobariya1801@users.noreply.github.com>
2026-09-04 18:07:04 -0700

    Add: combination-target-sum - submission-1

---
+++ Data Structures & Algorithms/combination-target-sum/submission-1.cpp
@@ -0,0 +1,60 @@
+class Solution {
+private:
+    // void solve(int i, int n, vector<int> &nums, int sum,int target,vector<vector<int>> &ans,vector<int> &temp){
+
+    //     if(i == n){
+    //         if(sum == target){
+    //             ans.push_back(temp);
+    //         }
+    //         return;
+    //     }
+    //     if(sum > target) return;
+
+    //     // pick
+    //     if(nums[i] <= target){
+    //         sum += nums[i];
+    //         temp.push_back(nums[i]);
+    //         solve(i,n,nums,sum,target, ans,temp);
+    //         sum = sum - nums[i];
+    //         temp.pop_back();
+    //     }
+        
+
+    //     solve(i+1,n,nums,sum,target,ans,temp);
+
+    // }
+    void solve(int i, int n, vector<int> &nums,int target,vector<vector<int>> &ans,vector<int> &temp){
+
+        if(i == n){
+            if(target == 0){
+                ans.push_back(temp);
+            }
+            return;
+        }
+
+        if(nums[i] <= target){
+            temp.push_back(nums[i]);
+            solve(i,n,nums,target-nums[i],ans,temp);
+            temp.pop_back();
+        }
+
+        solve(i+1,n,nums,target,ans,temp);
+
+    }
+
+
+public:
+    vector<vector<int>> combinationSum(vector<int>& nums, int target) {
+
+        int n = nums.size();
+
+        vector<vector<int>> ans;
+        vector<int> temp;
+
+        //solve(0,n,nums,0,target,ans,temp);
+        solve(0,n,nums,target,ans,temp);
+
+        return ans;
+        
+    }
+};
