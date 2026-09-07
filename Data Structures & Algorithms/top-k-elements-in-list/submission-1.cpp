56b7bb2292424cb4d8cf9c68f707203ff410d8f1
Harsh Dobariya <84761837+HarshDobariya1801@users.noreply.github.com>
2026-09-07 11:43:33 -0700

    Add: top-k-elements-in-list - submission-1

---
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-1.cpp
@@ -0,0 +1,66 @@
+class Solution {
+// private:
+//     struct node{
+//         int num;
+//         int freq;
+
+//         node(int a, int b){
+//             num = a;
+//             freq = b;
+//         }
+//     };
+
+//     struct comp{
+//         bool operator()(node a,node b){
+//             return a.freq < b.freq;
+//         }
+//     };
+
+
+public:
+    vector<int> topKFrequent(vector<int>& nums, int k) {
+
+        int n = nums.size();
+        map<int,int> mp;
+
+        for(int i = 0; i < n; i++){
+            mp[nums[i]]++;
+        }
+
+        priority_queue<pair<int,int>> pq;
+
+        for(auto it:mp){
+            pq.push({it.second,it.first});
+        }
+
+        vector<int> ans;
+
+        while(k--){
+            ans.push_back(pq.top().second);
+            pq.pop();
+        }
+
+        return ans;
+
+        // for(int i = 0; i < n; i++){
+        //     mp[nums[i]]++;
+        // }
+
+        // priority_queue<node,vector<node>, comp> q;
+
+        // for(auto it: mp){
+        //     q.push(node(it.first,it.second));
+        // }
+
+        // vector<int> ans;
+
+        // while(k--){
+        //     node temp = q.top();
+        //     q.pop();
+        //     ans.push_back(temp.num);
+        // }
+
+        // return ans;
+        
+    }
+};
