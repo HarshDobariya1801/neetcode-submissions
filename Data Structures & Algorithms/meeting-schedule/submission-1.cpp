33ab39147a92dbdf5ec31e386a625ce12ef22419
Harsh Dobariya <84761837+HarshDobariya1801@users.noreply.github.com>
2026-09-10 17:32:37 -0700

    Add: meeting-schedule - submission-1

---
+++ Data Structures & Algorithms/meeting-schedule/submission-1.cpp
@@ -0,0 +1,32 @@
+/**
+ * Definition of Interval:
+ * class Interval {
+ * public:
+ *     int start, end;
+ *     Interval(int start, int end) {
+ *         this->start = start;
+ *         this->end = end;
+ *     }
+ * }
+ */
+
+class Solution {
+private:
+    static bool comp(const Interval &a, const Interval &b){
+        return a.start < b.start;
+    }
+public:
+    bool canAttendMeetings(vector<Interval>& intervals) {
+
+        sort(intervals.begin(),intervals.end(),comp);
+
+        int n = intervals.size();
+
+        for(int i = 1; i < n; i++){
+            if(intervals[i].start < intervals[i-1].end) return false;
+        }
+
+        return true;
+        
+    }
+};
