9b160bf6b47c1976b35ea9aebb2406cd29a5126c
Harsh Dobariya <84761837+HarshDobariya1801@users.noreply.github.com>
2026-09-11 17:47:06 -0700

    Add: kth-largest-integer-in-a-stream - submission-0

---
+++ Data Structures & Algorithms/kth-largest-integer-in-a-stream/submission-0.cpp
@@ -0,0 +1,28 @@
+class KthLargest {
+    priority_queue<int,vector<int>, greater<int>> pq;
+    int k;
+public:
+    KthLargest(int k, vector<int>& nums) {
+        // this -> k = class member
+        // this -> k = k (constructor k)
+        this -> k = k;
+
+        for(auto it : nums){
+            pq.push(it);
+            if(pq.size() > k){
+                pq.pop();
+            }
+        }
+        
+    }
+    
+    int add(int val) {
+        pq.push(val);
+
+        if(pq.size() > k){
+            pq.pop();
+        }
+        
+        return pq.top();
+    }
+};
